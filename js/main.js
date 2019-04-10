d8b42faafc1e84223ba823b7137fe5cf4c45b8f7
joanne070 <joanne070@gmail.com>
2019-04-10 12:08:45 +0800

    Update main.js
    Submit button update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $form = $('form#signup-form'),
     url = 'https://script.google.com/macros/s/AKfycbw_MAJhTUi2xl2wK3YhANZrSalCXVs4fZogp_A_Au1Qk91vgTea/exec'
 
-$('#submit-form').on('click', function(e) {
+$('#submit').on('click', function(e) {
   e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
